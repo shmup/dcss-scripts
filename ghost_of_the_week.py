ce9b956228e794e415c007e18bcdba33f1cbc61b
Jared Miller <jtmiller@gmail.com>
2016-07-10 19:18:28 -0400

    gotw.py

---
+++ ghost_of_the_week.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# https://github.com/crawl/sequell/blob/master/docs/api.md
+
+import json
+import math
+import urllib.request
+
+term='https://loom.shalott.org/api/sequell/ldb?term='
+learndb='https://loom.shalott.org/api/sequell/ldb?search='
+listgame='https://loom.shalott.org/api/sequell/game?q='
+query = '!lg * week !boring s=killer killer=~ghost'
+params = '+'.join(query.split(' '))
+result = urllib.request.urlopen(listgame + params).read()
+data = json.loads(result.decode())
+
+print('Searching for: ' + learndb + params)
+
+motd = """
+                     _______           _______  _______ _________
+                    (  ____ \\|\\     /|(  ___  )(  ____ \\\\__   __/
+                    | (    \\/| )   ( || (   ) || (    \\/   ) (
+                    | |      | (___) || |   | || (_____    | |
+                    | | ____ |  ___  || |   | |(_____  )   | |
+                    | | \\_  )| (   ) || |   | |      ) |   | |
+                    | (___) || )   ( || (___) |/\\____) |   | |
+                    (_______)|/     \\|(_______)\\_______)   )_(
+
+ _______  _______   _________          _______             _______  _______  _
+(  ___  )(  ____ \\  \\__   __/|\\     /|(  ____ \\  |\\     /|(  ____ \\(  ____ \\| \\    /\\
+| (   ) || (    \\/     ) (   | )   ( || (    \\/  | )   ( || (    \\/| (    \\/|  \\  / /
+| |   | || (__         | |   | (___) || (__      | | _ | || (__    | (__    |  (_/ /
+| |   | ||  __)        | |   |  ___  ||  __)     | |( )| ||  __)   |  __)   |   _ (
+| |   | || (           | |   | (   ) || (        | || || || (      | (      |  ( \\ \\
+| (___) || )           | |   | )   ( || (____/\\  | () () || (____/\\| (____/\\|  /  \\ \\
+(_______)|/            )_(   |/     \\|(_______/  (_______)(_______/(_______/|_/    \\/
+"""
+
+gotw = data['data'][0]
+
+player = gotw[0].split("'")[0].strip()
+
+congrats = "Congratulations " + player + "! " + str(gotw[1]) + " kills!"
+
+motd += "\r\n\r\n\r\n\r\n"
+motd += congrats.center(85, ' ')
+motd += "\r\n\r\n"
+motd += ('http://crawl.akrasiac.org/scoring/players/' + player + '.html').center(85, ' ')
+
+print(motd)

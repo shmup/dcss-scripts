d08c56e86c21d612252193dec788e91c2db44a34
Jared Miller <jared.miller@northpointeinc.com>
2016-07-28 15:58:15 -0400

    a thing to output dcss tags

---
+++ timeline.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# usage: ./timeline.sh > out.txt
+
+{
+    cd ~/code/crawl
+    git pull
+} &> /dev/null
+
+echo DCSS tags generated on $(date "+%Y-%m-%d %H:%M:%S")
+echo 
+
+git log --date-order --graph --tags --simplify-by-decoration --pretty=format:'%ai %h %d'
